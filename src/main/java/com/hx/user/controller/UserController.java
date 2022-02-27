255e836b9a7f92912815dfa6ab1d0c734d19ef50
hxtome <hxtome@163.com>
2022-02-28 00:36:16 +0800

    数据库相关配置

--- src/main/java/com/hx/user/controller/UserController.java
+++ src/main/java/com/hx/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.hx.user.controller;
 
 import com.hx.entity.User;
 import com.hx.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +14,20 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserController {
+    Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private IUserService userService;
 
     @RequestMapping(value="/list", method={RequestMethod.GET,RequestMethod.POST})
     public ModelAndView getUserInfo(HttpServletRequest request){
         String name = request.getParameter("name");
+        log.error("eeror");
         System.out.println("111");
-        User user  =  userService.getUser(new User());
-        return new ModelAndView("user");
+        User param = new User();
+        param.setId(1);
+        User user  =  userService.selectById(param);
+        ModelAndView mav=new ModelAndView("user");
+            mav.getModel().put("name", user.getName());
+        return mav;
     }
 }
--- src/main/webapp/WEB-INF/view/user.jsp
+++ src/main/webapp/WEB-INF/view/user.jsp
@@ -1,5 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD//XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <body>
-<h2>userinfo!</h2>
+<h2>${name}</h2>
 </body>
+
 </html>
